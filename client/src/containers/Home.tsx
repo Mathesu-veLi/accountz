2723da5e493c9ab01a509bd13300e2e7e0bbb8b1
Matheus <matheuslevit@gmail.com>
2024-02-11 00:55:08 -0300

    creates a div to add a image of the site in the future

--- client/src/containers/Home.tsx
+++ client/src/containers/Home.tsx
@@ -7,17 +7,16 @@ export function Home() {
     <div className="flex justify-center">
       <div className="w-11/12 mx-10">
         {passwords.map((password) => (
-          <div key={password.website}>
-            <div>
-              <h1 className="text-lg font-semibold tracking-wider">
+          <div key={password.website} className="flex gap-5">
+            <div className="p-4 border rounded-sm w-16 h-16 flex justify-center items-center">
+              ?
+            </div>
+            <div className="h-16">
+              <h1 className="font-semibold tracking-wider">
                 {password.website}
               </h1>
-              <p className="text-sm text-gray-400">
-                {password.username}
-              </p>
-              <p className="text-sm text-gray-400">
-                {password.email}
-              </p>
+              <p className="text-sm text-gray-400">{password.username}</p>
+              <p className="text-sm text-gray-400">{password.email}</p>
             </div>
           </div>
         ))}
