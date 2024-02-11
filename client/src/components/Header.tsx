173dcbc0a7855b4a4638c2fc7d4cd2487d9afb03
Matheus <matheuslevit@gmail.com>
2024-02-10 23:34:17 -0300

    add a link to the add password page and to the home

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import { PiGearSixLight, PiPlusCircleLight } from 'react-icons/pi';
+import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
     <div className="flex justify-center items-center mb-4">
       <div className="flex justify-between w-11/12 py-4">
         <PiGearSixLight className="w-7 h-7" />
-        <h1 className="font-light">Password</h1>
-        <PiPlusCircleLight className="w-7 h-7" />
+        <h1 className="font-light"><Link to="/">Password</Link></h1>
+        <Link to="/password">
+          <PiPlusCircleLight className="w-7 h-7" />
+        </Link>
       </div>
     </div>
   );
