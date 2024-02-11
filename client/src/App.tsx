f3c6aead822f67356336af79d77ffce024fd6596
Matheus <matheuslevit@gmail.com>
2024-02-11 10:50:00 -0300

    rename container of add password page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Password } from './containers/Password';
 import { Header } from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AddPassword } from './containers/AddPassword';
 
 function App() {
   const routes = [
@@ -13,6 +14,10 @@ function App() {
     },
     {
       path: '/password',
+      element: <AddPassword />,
+    },
+    {
+      path: '/password/:id',
       element: <Password />,
     },
   ];
