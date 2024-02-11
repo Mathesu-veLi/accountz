a3af1b55b72030578adde8c0a6eb246513be1278
Matheus <matheuslevit@gmail.com>
2024-02-10 23:04:19 -0300

    creates a page to save a password

--- client/src/containers/Password.tsx
+++ client/src/containers/Password.tsx
@@ -1,3 +1,89 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  username: z.string(),
+  email: z.string().min(1).email('Email not valid'),
+  password: z.string().min(1),
+});
+
+type TFormSchema = z.infer<typeof formSchema>;
+
 export function Password() {
-    return <h1>Password page</h1>
+  const form = useForm<TFormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  //function addPasswordToStore() {}
+
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="flex flex-col justify-between items-center border rounded-sm shadow-sm p-4 w-11/12">
+        <Form {...form}>
+          <form
+            action=""
+            className="flex flex-col justify-between items-center gap-5"
+          >
+            <h1>Add</h1>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
