1db9aeccd321a73d045e084317555a156e8717b1
Matheus <matheuslevit@gmail.com>
2024-02-10 22:58:40 -0300

    set width to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,10 +2,12 @@ import { PiGearSixLight, PiPlusCircleLight } from 'react-icons/pi';
 
 export function Header() {
   return (
-    <div className="flex justify-between p-6">
-      <PiGearSixLight className="w-7 h-7" />
-      <h1 className="font-light">Password</h1>
-      <PiPlusCircleLight className="w-7 h-7" />
+    <div className="flex justify-center items-center mb-4">
+      <div className="flex justify-between w-11/12 py-4">
+        <PiGearSixLight className="w-7 h-7" />
+        <h1 className="font-light">Password</h1>
+        <PiPlusCircleLight className="w-7 h-7" />
+      </div>
     </div>
   );
 }
