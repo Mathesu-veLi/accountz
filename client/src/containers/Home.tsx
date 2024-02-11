4a1837bab11494bfc8c106efabe6795edd28733b
Matheus <matheuslevit@gmail.com>
2024-02-11 00:50:45 -0300

    shows registered accounts in the home page

--- client/src/containers/Home.tsx
+++ client/src/containers/Home.tsx
@@ -1,3 +1,27 @@
+import { usePasswordStore } from '@/store/usePasswordStore';
+
 export function Home() {
-  return <h1>Home</h1>;
+  const { passwords } = usePasswordStore();
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-11/12 mx-10">
+        {passwords.map((password) => (
+          <div key={password.website}>
+            <div>
+              <h1 className="text-lg font-semibold tracking-wider">
+                {password.website}
+              </h1>
+              <p className="text-sm text-gray-400">
+                {password.username}
+              </p>
+              <p className="text-sm text-gray-400">
+                {password.email}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
