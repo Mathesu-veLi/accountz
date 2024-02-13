f20550bcd81efaf17126c5838238173a12489f0b
Matheus <matheuslevit@gmail.com>
2024-02-12 23:29:33 -0300

    redirects user to home page if password does not exist

--- client/src/containers/Password.tsx
+++ client/src/containers/Password.tsx
@@ -1,5 +1,21 @@
-import { AddPassword } from './AddPassword';
+import { IPassword } from '@/interfaces/IPassword';
+import { usePasswordStore } from '@/store/usePasswordStore';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export function Password() {
-  return <h1>Password</h1>;
+  const { id } = useParams();
+  const { passwords } = usePasswordStore();
+  const [password, setPassword] = useState<IPassword>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const password = passwords.filter((password) => password.id == id)[0];
+    if (!password) {
+      toast.error('Password id not exists');
+      navigate('/');
+    } else setPassword(password);
+  }, [id, navigate, passwords]);
+
 }
