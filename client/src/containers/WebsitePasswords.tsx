7a47cdbdebfd67a1c90242728ba95fe0e8671773
Matheus <matheuslevit@gmail.com>
2024-02-14 19:51:32 -0300

    fix password existence validation

--- client/src/containers/WebsitePasswords.tsx
+++ client/src/containers/WebsitePasswords.tsx
@@ -2,6 +2,7 @@ import { usePasswordStore } from '@/store/usePasswordStore';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { IoMdArrowDropright } from 'react-icons/io';
 import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
 export function WebsitePasswords() {
   const websiteParam = useParams().website as string;
@@ -10,25 +11,28 @@ export function WebsitePasswords() {
 
   const navigate = useNavigate();
 
-  if (!websitePasswords) {
-    toast.error('Website not registered');
-    return navigate('/');
-  }
+  useEffect(() => {
+    if (!websitePasswords || !websitePasswords.length) {
+      toast.error('Website not registered');
+      return navigate('/');
+    }
+  }, [navigate, websitePasswords]);
 
   return (
     <div className="flex justify-center">
       <div className="mx-10 flex justify-center items-center flex-col gap-10 mt-10 border p-8 lg:p-10 rounded-sm">
         <h1 className="text-lg font-semibold">{websiteParam}</h1>
-        {websitePasswords.map((password, index) => (
-          <Link
-            to={`/password/${websiteParam}/${index}`}
-            key={index}
-            className="flex p-4 border rounded-sm justify-between items-center w-full gap-5"
-          >
-            <h1>{password?.email}</h1>
-            <IoMdArrowDropright />
-          </Link>
-        ))}
+        {websitePasswords &&
+          websitePasswords.map((password, index) => (
+            <Link
+              to={`/password/${websiteParam}/${index}`}
+              key={index}
+              className="flex p-4 border rounded-sm justify-between items-center w-full gap-5"
+            >
+              <h1>{password?.email}</h1>
+              <IoMdArrowDropright />
+            </Link>
+          ))}
       </div>
     </div>
   );
