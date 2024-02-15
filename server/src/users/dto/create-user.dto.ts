aae25cd755ceb463f253cb968447b89146e7ef4f
Matheus <matheuslevit@gmail.com>
2024-02-15 12:39:39 -0300

    validates user data

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @MinLength(4)
+  @MaxLength(20)
   password: string;
 }
