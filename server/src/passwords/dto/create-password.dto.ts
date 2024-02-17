c1680e6c00ecac6946219c79a664647acea0c3fb
Matheus <matheuslevit@gmail.com>
2024-02-17 17:03:19 -0300

    validate passwords dto

--- server/src/passwords/dto/create-password.dto.ts
+++ server/src/passwords/dto/create-password.dto.ts
@@ -1,7 +1,18 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreatePasswordDto {
+  @IsNotEmpty()
   userId: number;
+
+  @IsNotEmpty()
   website: string;
+
+  @IsNotEmpty()
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
