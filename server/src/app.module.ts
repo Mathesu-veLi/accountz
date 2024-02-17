a1d86d27ddfffee5edcf4283c8c3e696a119dd89
Matheus <matheuslevit@gmail.com>
2024-02-17 17:25:15 -0300

    creates a middleware to validate if consumer is logged in

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,29 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PasswordsModule } from './modules/passwords/passwords.module';
 import { TokensModule } from './modules/tokens/tokens.module';
+import { LoginRequiredMiddleware } from './middlewares/loginRequired';
 
 @Module({
   imports: [UsersModule, PrismaModule, PasswordsModule, TokensModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoginRequiredMiddleware)
+      .forRoutes(
+        { path: 'users/:id', method: RequestMethod.PATCH },
+        { path: 'users/:id', method: RequestMethod.DELETE },
+      );
+  }
+}
