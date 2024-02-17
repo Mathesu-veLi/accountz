c1a4dd8aea4f3b058b686b023ce4f610add22a93
Matheus <matheuslevit@gmail.com>
2024-02-17 17:20:32 -0300

    creates a function to verify if password is valid

--- server/src/utils/passwordUtils.ts
+++ server/src/utils/passwordUtils.ts
@@ -1,5 +1,12 @@
-import { hashSync } from 'bcryptjs';
+import { compareSync, hashSync } from 'bcrypt';
 
 export function generatePasswordHash(password: string): string {
   return hashSync(password, 10);
+}
+
+export function passwordIsValid(
+  password: string,
+  password_hash: string,
+): boolean {
+  return compareSync(password, password_hash);
 }
