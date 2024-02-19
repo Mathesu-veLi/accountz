4a918107e6ddf0465d4854ae1523af02f42786c1
Matheus <matheuslevit@gmail.com>
2024-02-19 20:51:52 -0300

    add a account dropdown in the header

---
+++ client/src/components/AccountDropdown.tsx
@@ -0,0 +1,40 @@
+import { PiGearSixLight } from 'react-icons/pi';
+import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Link } from 'react-router-dom';
+
+export function AccountDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <PiGearSixLight className="w-7 h-7" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup className="flex gap-2">
+          <DropdownMenuItem>
+            <Link to="/login" className="px-4 py-2">
+              Login
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link to="/register" className="px-4 py-2">
+              Register
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,20 @@
-import { PiGearSixLight, PiPlusCircleLight } from 'react-icons/pi';
+import { PiPlusCircleLight } from 'react-icons/pi';
 import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import { AccountDropdown } from './AccountDropdown';
 
 export function Header() {
   return (
     <div className="flex justify-center items-center mb-4">
       <div className="flex justify-between w-11/12 py-4">
-        <PiGearSixLight className="w-7 h-7" />
-        <h1 className="font-light"><Link to="/">Password</Link></h1>
+        <AccountDropdown />
+        <h1 className="font-light">
+          <Link to="/">Password</Link>
+        </h1>
         <Link to="/password">
-          <PiPlusCircleLight className="w-7 h-7" />
+          <Button variant="ghost">
+            <PiPlusCircleLight className="w-7 h-7" />
+          </Button>
         </Link>
       </div>
     </div>
