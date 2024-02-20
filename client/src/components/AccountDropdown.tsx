6bf0d86c1221c8154f86e2e80dfc096f7fff5a50
Matheus <matheuslevit@gmail.com>
2024-02-20 20:48:43 -0300

    show a logout button if the user is logged in

--- client/src/components/AccountDropdown.tsx
+++ client/src/components/AccountDropdown.tsx
@@ -9,9 +9,16 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useUserStore } from '@/store/useUserStore';
+import { toast } from 'react-toastify';
 
 export function AccountDropdown() {
+  const { name } = useUserStore().user;
+  const { reset } = useUserStore();
+
+  const navigate = useNavigate();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -20,19 +27,35 @@ export function AccountDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>{!name ? 'My Account' : name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuGroup className="flex gap-2">
-          <DropdownMenuItem>
-            <Link to="/login" className="px-4 py-2">
-              Login
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link to="/register" className="px-4 py-2">
-              Register
-            </Link>
-          </DropdownMenuItem>
+        <DropdownMenuGroup>
+          {!name ? (
+            <div className="flex gap-2">
+              <DropdownMenuItem>
+                <Link to="/login" className="px-4 py-2">
+                  Login
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link to="/register" className="px-4 py-2">
+                  Register
+                </Link>
+              </DropdownMenuItem>
+            </div>
+          ) : (
+            <div>
+              <DropdownMenuItem
+                onClick={() => {
+                  toast('User successfully logged out');
+                  navigate('/');
+                  reset();
+                }}
+              >
+                Logout
+              </DropdownMenuItem>
+            </div>
+          )}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
