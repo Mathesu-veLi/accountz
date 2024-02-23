9e21d19d3d71cab795cda27cb69947f0ea199833
Matheus <matheuslevit@gmail.com>
2024-02-22 21:43:45 -0300

    show the favicon of the website

--- client/src/components/WebsitePasswordCard.tsx
+++ client/src/components/WebsitePasswordCard.tsx
@@ -1,13 +1,14 @@
-import { IPassword } from '@/interfaces/IPassword';
+import { IAccount } from '@/interfaces/IAccount';
 import { Link } from 'react-router-dom';
 
 interface IProps {
   website: string;
-  password: IPassword;
+  password: IAccount;
   quantityOfPasswords: number;
 }
 
 export function WebsitePasswordCard(props: IProps) {
+  console.log(`${props.password.websiteUrl}/favicon.ico`);
   return (
     <Link
       to={`/password/${props.website}`}
@@ -15,14 +16,19 @@ export function WebsitePasswordCard(props: IProps) {
       className="flex gap-5"
     >
       <div className="p-4 border rounded-sm w-16 h-16 flex justify-center items-center">
-        ?
+        <img
+          src={`https://${props.password.websiteUrl}/favicon.ico`}
+          className="w-full h-full"
+        />
       </div>
       <div className="h-16">
         <h1 className="font-semibold tracking-wider">{props.website}</h1>
         <p className="text-sm text-gray-400">{props.password.username}</p>
         <div className="flex justify-center items-center text-sm text-gray-400 gap-3">
           <p>{props.password.email}</p>
-          {props.quantityOfPasswords > 1 && <span>+{props.quantityOfPasswords - 1}</span>}
+          {props.quantityOfPasswords > 1 && (
+            <span>+{props.quantityOfPasswords - 1}</span>
+          )}
         </div>
       </div>
     </Link>
